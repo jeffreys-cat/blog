0374cee55ea4b93d9447ad9dc453926dce49844f
Jeffrey <color.dove@gmail.com>
2023-06-17 18:55:32 +0800

    (app) fix eslint error

--- app/(main)/Resume.tsx
+++ app/(main)/Resume.tsx
@@ -84,7 +84,7 @@ export function Resume() {
             <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
               <Image
                 src={role.logo}
-                alt={role.company}
+                alt="company"
                 className="h-8 w-8 rounded-full"
                 unoptimized
               />
