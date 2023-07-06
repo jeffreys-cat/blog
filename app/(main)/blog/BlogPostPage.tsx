1aa967a1793e1961a8009219d2e7980ec5a414f8
Jeffrey <color.dove@gmail.com>
2023-07-07 00:53:24 +0800

    (feat) limit img max size

--- app/(main)/blog/BlogPostPage.tsx
+++ app/(main)/blog/BlogPostPage.tsx
@@ -68,7 +68,8 @@ export function BlogPostPage({
                     alt=""
                     className="select-none"
                     unoptimized
-                    fill
+                    width={576}
+                    height={1024}
                     aria-hidden={true}
                   />
                 </div>
