ddb234284e0b822b219c41c7faa2a8245169b7f0
Jeffrey <color.dove@gmail.com>
2023-07-20 21:40:31 +0800

    update

--- config/nav.ts
+++ config/nav.ts
@@ -2,8 +2,7 @@ export const navigationItems = [
   { href: '/', text: '首页' },
   { href: '/blog', text: '博客' },
   { href: '/about', text: '阅读' },
-  // { href: '/girlfriend', text: '晒美女女友' },
+  { href: '/girlfriend', text: '晒美女女友' },
   { href: '/guestbook', text: '留言墙' },
-  { href: '/ama', text: 'AMA 咨询' },
   // { href: '/about', text: '关于' },
 ]
